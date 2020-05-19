80fc57508165e36a8b37dd979b38e8032e0ce938
maxjf1 <maxwellsouzacarvalho@gmail.com>
2020-05-19 00:26:34 -0300

    FIX documentation

--- src/files_ms_client/client.py
+++ src/files_ms_client/client.py
@@ -5,15 +5,14 @@ import json
 files_url = (getenv('FILES_URL') or '')
 file_key = 'file'
 
-
+# Upload a file to the server
 def upload(file: str, url: str = files_url):
-    # Upload a file to the server
     files = {file_key: open(file, 'rb')}
     r = post(f'{url}/files', files=files)
     return r.json()[file_key]
 
-
+# Download a file from the server
 def download(file: str, path: str, url: str = files_url):
-    # Download a file from the server
+    
     r = get(f'{url}/files/{file}')
     open(path, 'wb').write(r.content)
