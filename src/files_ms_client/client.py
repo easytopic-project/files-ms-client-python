1386e87aba8caed302d5ed44ec738d30478e8198
Maxwell Souza <maxwellsouzacarvalho@gmail.com>
2020-05-27 04:24:02 -0300

    FIX typo and documentation

--- src/files_ms_client/client.py
+++ src/files_ms_client/client.py
@@ -6,20 +6,23 @@ from mimetypes import guess_type
 files_url = getenv('FILES_URL', '')
 file_key = 'file'
 
-# Upload a file to the server
-def upload(file: str, delete: bool = false, url: str = files_url):
+
+def upload(file: str, delete: bool = False, url: str = files_url):
+    """Upload a file to the server"""
     files = {file_key: (file, open(file, 'rb'), guess_type(file)[0])}
     r = req.post(f'{url}/files', files=files)
-    if delete: # Delete the file
+    if delete:  # Delete the file
         remove(file)
     return r.json()[file_key]
 
-# Download a file from the server
+
 def download(file: str, path: str, url: str = files_url):
+    """Download a file from the server"""
     r = req.get(f'{url}/files/{file}')
     open(path, 'wb').write(r.content)
 
-# Delete a file from the server
+
 def delete(file: str, url: str = files_url):
+    """Delete a file from the server"""
     r = req.delete(f'{url}/files/{file}')
     return r.json()
